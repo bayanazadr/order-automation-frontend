dd7969d9e50f3aa9ca51c4a06be1e882ab247cbd
DinaraIsmagambetova <190103266@stu.sdu.edu.kz>
2023-03-11 02:28:10 +0600

    add promotion card component

---
+++ src/components/PromotionCard.jsx
@@ -0,0 +1,9 @@
+const PromotionCard = () => {
+    return ( 
+        <div className="flex items-center justify-center w-full">
+            <img className="w-[360px] rounded-2xl" src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'></img>
+        </div>
+     );
+}
+ 
+export default PromotionCard;
