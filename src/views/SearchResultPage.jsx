712b9ade665a18c5ed07a4badc5f4be50ed92731
bayanazadr <126002353+bayanazadr@users.noreply.github.com>
2023-05-06 16:55:55 +0600

    add routing to search result

--- src/views/SearchResultPage.jsx
+++ src/views/SearchResultPage.jsx
@@ -3,8 +3,20 @@ import SearchItem from "../components/SearchItem";
 import SearchPageHeader from "../components/SearchPageHeader";
 import {useEffect, useState} from "react";
 import axios from 'axios';
+import {useLocation, createSearchParams, useNavigate} from "react-router-dom";
 
 const SearchResultPage = () => {
+  const navigate = useNavigate()
+  const navigateFoodDetails = (uuid) => 
+  {
+    const params = [
+      ['id', uuid]
+    ]
+    navigate({
+      pathname: '/food-details',
+      search: `?${createSearchParams(params)}`
+    })
+}
     const [searchValue, setsearchValue] = useState([])
     const fetchDetails = async(value) => {
       const link = `https://order-automation-debug-server.onrender.com/api/get-dish-by-filter`;
@@ -30,7 +42,9 @@ const SearchResultPage = () => {
             <SearchPageHeader count={searchValue.length} onSearch={handleSearch}/>
             <div className="w-full h-[100vh] rounded-t-3xl bg-[#F7F7F7] px-6 flex flex-col space-y-10">
             {searchValue.map(dish => (
-                <SearchItem price={dish.price} img={ JSON.parse(dish.pictures)[0]} key={dish.uuid} name={dish.title}  />
+              <div onClick={() => navigateFoodDetails(dish.uuid)}>
+                <SearchItem  price={dish.price} img={ JSON.parse(dish.pictures)[0]} key={dish.uuid} name={dish.title}  />
+              </div>
             ))}
             </div>
             <Navbar/>
