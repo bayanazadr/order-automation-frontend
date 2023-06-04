ecb542600ba01676092afb1b753acbbdb4770170
bayanazadr <126002353+bayanazadr@users.noreply.github.com>
2023-06-05 00:44:16 +0600

    fix basket delete and total price

--- src/components/BasketItem.jsx
+++ src/components/BasketItem.jsx
@@ -1,54 +1,50 @@
 import trash from '../images/trash.png'
 import {useEffect, useState} from "react";
 
-const BasketItem = (data) => {
-    const [basket, setBasket] = useState({});
-    const [dishAmount, setDishAmount] = useState(1)
-    useEffect(() => {
-        setBasket(data.basket)
-    }, [])
+const BasketItem = ({ basket, onDeleteItem, onIncrementDishCount, onReduceDishCount }) => {
+  const [dishAmount, setDishAmount] = useState(basket.dishAmount || 1);
 
-    const deleteBasketItem = () => {
-        let baskets = JSON.parse(localStorage.getItem('basket'))
-        if (basket) {
-            baskets = baskets.filter(x => x.uuid !== basket.uuid)
-            localStorage.setItem('basket', JSON.stringify(baskets))
-        }
+  useEffect(() => {
+    setDishAmount(basket.dishAmount || 1);
+  }, [basket]);
+
+  const deleteBasketItem = () => {
+    onDeleteItem(basket.uuid);
+  };
+
+  const incrementDishCount = () => {
+    let count = Number(dishAmount)
+    setDishAmount(++count)
+    onIncrementDishCount(basket.price);
+  };
+
+  const reduceDishCount = () => {
+    let count = Number(dishAmount)
+    if (count > 1) {
+      setDishAmount(--count)
+      onReduceDishCount(basket.price);
+    } else {
+      deleteBasketItem();
     }
+  }
 
-    const incrementDishCount = () => {
-        let count = Number(dishAmount)
-        setDishAmount(++count)
-    }
-
-    const reduceDishCount = () => {
-        let count = Number(dishAmount)
-        if (count > 1) {
-            setDishAmount(--count)
-        } else {
-            deleteBasketItem();
-        }
-    }
-
-    return (
-        <div className='w-full h-20 flex justify-between items-center'>
-            <div className='flex flex-col items-start'>
-                <p className='text-base font-bold'>{basket.title}</p>
-                <p className='text-base'>{basket.price} tg</p>
-            </div>
-            <div className='bg-[#979797]/10 w-32 h-12 flex items-center justify-center rounded-lg'>
-                <button className='w-12 h-12 flex items-center justify-center'>
-                    <img src={trash} onClick={deleteBasketItem}/>
-                </button>
-                <button onClick={reduceDishCount} className='w-12 h-12 text-2xl font-semibold text-[#FB6D3A]'>-</button>
-
-                <p className='flex items-center justify-center w-10 h-12 text-base font-semibold'>{dishAmount}</p>
-                <button onClick={incrementDishCount} className='w-12 h-12 text-2xl font-semibold text-[#FB6D3A]'>+
-                </button>
-            </div>
-
-        </div>
-    );
+  return (
+    <div className='w-full h-20 flex justify-between items-center'>
+      <div className='flex flex-col items-start'>
+        <p className='text-base font-bold'>{basket.title}</p>
+        <p className='text-base'>{basket.price} tg</p>
+      </div>
+      <div className='bg-[#979797]/10 w-32 h-12 flex items-center justify-center rounded-lg'>
+        <button className='w-12 h-12 flex items-center justify-center'>
+          <img src={trash} onClick={deleteBasketItem} alt='Delete' />
+        </button>
+        <button onClick={reduceDishCount} className='w-12 h-12 text-2xl font-semibold text-[#FB6D3A]'>-</button>
+        <p className='flex items-center justify-center w-10 h-12 text-base font-semibold'>{dishAmount}</p>
+        <button onClick={incrementDishCount} className='w-12 h-12 text-2xl font-semibold text-[#FB6D3A]'>+</button>
+      </div>
+      
+    </div>
+  );
 }
 
 export default BasketItem;
--- src/views/Basket.jsx
+++ src/views/Basket.jsx
@@ -5,71 +5,88 @@ import {useEffect, useState} from 'react';
 
 const Basket = () => {
 
-    const [baskets, setBaskets] = useState([])
-    const [price, setPrice] = useState(0)
+  const [baskets, setBaskets] = useState([])
+  const [price, setPrice] = useState(0)
 
-    const initBasket = () => {
-        const basket = JSON.parse(localStorage.getItem('basket'));
+  const initBasket = () => {
+    const basket = JSON.parse(localStorage.getItem('basket'));
 
-        if (basket) {
-            setBaskets(basket)
-        }
+    if (basket) {
+      setBaskets(basket)
     }
+  }
 
-    useEffect(() => {
-        initBasket()
-    }, [])
+  useEffect(() => {
+    initBasket()
+  }, [])
 
-    useEffect(() => {
-        calculatePrice()
-    }, [baskets])
+  useEffect(() => {
+    calculatePrice()
+  }, [baskets])
 
-    const calculatePrice = () => {
-        let sum = 0;
-        baskets.forEach(item => {
-            sum += Number(item.price)
-        })
-        console.log('ar3C1 :: sum: ', sum)
-        setPrice(sum)
-    }
+  const calculatePrice = () => {
+    let sum = 0;
+    baskets.forEach(item => {
+      sum += Number(item.price)
+    })
+    console.log('ar3C1 :: sum: ', sum)
+    setPrice(sum)
+  }
 
-    return (
-        <div className='w-full h-[100vh] bg-[#503E9D] flex flex-col justify-between'>
-            <div className='bg-white px-6 rounded-b-3xl'>
-                <div className='pb-20'>
-                    <BasketHeader/>
-                    <div className='w-full flex items-center justify-between'>
-                        <p className='text-left text-xl font-bold text-[#FB6D3A] py-5'>Basket</p>
-                        <div className='w-20 h-6 bg-[#FB6D3A] flex items-center justify-center rounded'>
-                            <p className='text-white text-xs font-medium'>25-30 min</p>
-                        </div>
-                    </div>
-                    <div className='w-full flex flex-col items-start space-y-5'>
-                        {baskets.map((basket) => (
-                                <BasketItem basket={basket}/>
-                            )
-                        )}
+  const deleteBasketItem = (uuid) => {
+    const updatedBasket = baskets.filter((item) => item.uuid !== uuid);
+    setBaskets(updatedBasket);
+    localStorage.setItem('basket', JSON.stringify(updatedBasket));
+  };
 
-                        <div
-                            className='space-x-3 flex items-center justify-center text-[#503E9D] text-base font-semibold h-16'>
-                            <p className='text-3xl pb-2'>+</p>
-                            <p className='text-lg'>Add more items</p>
-                        </div>
-                    </div>
-                </div>
-                <div className='w-full h-20 flex justify-between items-center mb-10'>
-                    <div className='flex flex-col items-start'>
-                        <p className='text-base font-bold'>Promo code</p>
-                        <p className='text-base'>HXFWO</p>
-                    </div>
-                    <div className='bg-[#979797]/10 h-12 flex items-center justify-center rounded-xl'>
-                        <button className='px-2 text-3xl pb-2 font-semibold text-[#503E9D]'>+</button>
-                    </div>
-                </div>
+  const incrementDishCount = (itemPrice) => {
+    setPrice((prevPrice) => prevPrice + Number(itemPrice));
+  };
+
+  const reduceDishCount = (itemPrice) => {
+    setPrice((prevPrice) => prevPrice - Number(itemPrice));
+  };
+
+  return (
+    <div className='w-full h-[100vh] bg-[#503E9D] flex flex-col justify-between'>
+      <div className='bg-white px-6 rounded-b-3xl'>
+        <div className='pb-20'>
+          <BasketHeader/>
+          <div className='w-full flex items-center justify-between'>
+            <p className='text-left text-xl font-bold text-[#FB6D3A] py-5'>Basket</p>
+            <div className='w-20 h-6 bg-[#FB6D3A] flex items-center justify-center rounded'>
+              <p className='text-white text-xs font-medium'>25-30 min</p>
             </div>
-            <BasketPrice data={price}/>
+          </div>
+          <div className='w-full flex flex-col items-start space-y-5'>
+            {baskets.map((basket) => (
+              <BasketItem
+                key={basket.uuid}
+                basket={basket}
+                onDeleteItem={deleteBasketItem}
+                onIncrementDishCount={incrementDishCount}
+                onReduceDishCount={reduceDishCount}
+              />
+            ))}
+            <div className='space-x-3 flex items-center justify-center text-[#503E9D] text-base font-semibold h-16'>
+              <p className='text-3xl pb-2'>+</p>
+              <p className='text-lg'>Add more items</p>
+            </div>
+          </div>
         </div>
-    );
+        <div className='w-full h-20 flex justify-between items-center mb-10'>
+          <div className='flex flex-col items-start'>
+            <p className='text-base font-bold'>Promo code</p>
+            <p className='text-base'>HXFWO</p>
+          </div>
+          <div className='bg-[#979797]/10 h-12 flex items-center justify-center rounded-xl'>
+            <button className='px-2 text-3xl pb-2 font-semibold text-[#503E9D]'>+</button>
+          </div>
+        </div>
+      </div>
+      <BasketPrice data={price}/>
+    </div>
+  );
 }
 
 export default Basket;
