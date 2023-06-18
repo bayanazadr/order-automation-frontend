c1011525e01356c555307015bb9c6e1dc82b1df1
Daryn <daryn200150@gmail.com>
2023-06-19 02:15:47 +0600

    fix navigateToHome function

--- src/components/PaymentHeader.jsx
+++ src/components/PaymentHeader.jsx
@@ -1,8 +1,16 @@
 import back from '../images/back.png'
 import { useNavigate } from 'react-router-dom';
+import {useEffect, useState} from "react";
 const PaymentHeader = () => {
+    const [tableId, setTableId]  = useState('');
     const navigate = useNavigate();
-    const navigateToHome = () => navigate('/')
+    const navigateToHome = () => navigate(`/${tableId}`)
+
+    useEffect(() => {
+        const id = JSON.parse(localStorage.getItem('table_id'));
+        setTableId(id);
+    }, [tableId])
+
     return ( 
         <div className='flex items-center w-full h-28 space-x-6'>
             <img src={back} onClick={navigateToHome}/>
