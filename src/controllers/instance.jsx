73cba2c8147aab27ad0bf196044405bb50c3a4a6
Daryn <daryn200150@gmail.com>
2023-06-19 02:14:58 +0600

    change api

--- src/controllers/instance.jsx
+++ src/controllers/instance.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://order-automation-debug-server.onrender.com/api',
+    baseURL: 'http://77.243.80.82:1313/api',
+    // baseURL: 'https://order-automation-debug-server.onrender.com/api',
     headers: {
         "Content-Type": "application/json",
     }
